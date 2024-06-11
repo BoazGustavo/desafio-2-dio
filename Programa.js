b82cd19cf16a0d89b8f8b7db8cb74b5c45448a94
GustavoBoaz <gustavo.boaz@hotmail.com>
2024-06-11 20:45:36 -0300

    20240611/GustavoBoaz/feature-DESAFIO 2 DIO

---
+++ Programa.js
@@ -0,0 +1,26 @@
+function calcularSaldoEVerNivel(vitorias, derrotas) {
+    const saldoVitorias = vitorias - derrotas;
+
+    let nivel = "";
+    if (vitorias < 10) {
+        nivel = "Ferro";
+    } else if (vitorias <= 20) {
+        nivel = "Bronze";
+    } else if (vitorias <= 50) {
+        nivel = "Prata";
+    } else if (vitorias <= 80) {
+        nivel = "Ouro";
+    } else if (vitorias <= 90) {
+        nivel = "Diamante";
+    } else if (vitorias <= 100) {
+        nivel = "Lendário";
+    } else {
+        nivel = "Imortal";
+    }
+
+    console.log(`O Herói tem de saldo de ${saldoVitorias} está no nível de ${nivel}`);
+}
+
+const vitorias = 75;
+const derrotas = 25;
+calcularSaldoEVerNivel(vitorias, derrotas);
